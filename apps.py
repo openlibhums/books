990060e83bfca9179a667286f543adbeb6b36040
Andy Byers <andy.byers@openlibhums.org>
2017-02-07 17:02:34 +0000

    MODELLING!

--- apps.py
+++ apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class TestingConfig(AppConfig):
-    name = 'testing'
+class BooksConfig(AppConfig):
+    name = 'books'
--- models.py
+++ models.py
@@ -1,3 +1,52 @@
-from django.db import models
+import uuid
+import os
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+fs = FileSystemStorage(location=settings.MEDIA_ROOT)
+
+
+def cover_images_upload_path(instance, filename):
+    try:
+        filename = str(uuid.uuid4()) + '.' + str(filename.split('.')[1])
+    except IndexError:
+        filename = str(uuid.uuid4())
+
+    path = "cover_images/"
+    return os.path.join(path, filename)
+
+
+class Book(models.Model):
+    prefix = models.CharField(max_length=20)
+    title = models.CharField(max_length=300)
+    subtitle = models.CharField(max_length=300, blank=True, null=True)
+    description = models.TextField()
+    pages = models.PositiveIntegerField()
+
+    contributors = models.ManyToManyField(Contributor)
+    is_edited_volume = models.BooleanField(default=False)
+
+    publisher_name = models.CharField(max_length=100)
+    publisher_loc = models.CharField(max_length=100)
+    cover = models.FileField(upload_to=cover_images_upload_path, null=True, blank=True, storage=fs)
+
+
+class Contributor(models.Model):
+    first_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100, blank=True, null=True)
+    last_name = models.CharField(max_length=100)
+
+    affiliation = models.TextField()
+    email = models.EmailField(blank=True, null=True)
+    sequence = models.PositiveIntegerField(default=10)
+
+
+class Format(models.Model):
+    format = models.ForeignKey(Book)
+
+    title = models.CharField(max_length=100)
+    filename = models.CharField(max_length=100)
+    sequence = models.PositiveIntegerField(default=10)
