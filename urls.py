53398c02b3917f5e75eba20892efbe3f29a7e0d4
Andy Byers <ajrbyers@gmail.com>
2017-09-05 18:15:34 +0100

    Added books url and set page title for index.

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@ from plugins.books import views
 
 urlpatterns = [
     url(r'^$', views.index, name='books_index'),
-    url(r'^book/(?P<book_id>\d+)/$', views.view_book, name='books_book'),
-    url(r'^book/(?P<book_id>\d+)/format/(?P<format_id>\d+)/$', views.download_format, name='books_download_format'),
+    url(r'^(?P<book_id>\d+)/$', views.view_book, name='books_book'),
+    url(r'^(?P<book_id>\d+)/format/(?P<format_id>\d+)/$', views.download_format, name='books_download_format'),
 
     url(r'^admin/$', views.admin, name='books_admin'),
     url(r'^admin/new/$', views.edit_book, name='books_new_book'),
