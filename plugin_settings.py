dc7676ac5b29f5642747dc1422a23556e76d54b0
Andy Byers <a.byers@bbk.ac.uk>
2019-08-14 13:44:36 +0100

    Updated chapter drop down.

--- plugin_settings.py
+++ plugin_settings.py
@@ -6,13 +6,16 @@ SHORT_NAME = 'books'
 MANAGER_URL = 'books_admin'
 JANEWAY_VERSION = "1.3.5"
 
-
 from utils import models
 
 
 def install():
-    new_plugin, created = models.Plugin.objects.get_or_create(name=SHORT_NAME, version=VERSION, enabled=True,
-                                                              press_wide=True)
+    new_plugin, created = models.Plugin.objects.get_or_create(
+        name=SHORT_NAME,
+        enabled=True,
+        press_wide=True,
+        defaults={'version': VERSION},
+    )
 
     if created:
         print('Plugin {0} installed.'.format(PLUGIN_NAME))
